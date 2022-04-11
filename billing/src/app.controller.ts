4fa38768ec58b826d174f857bcc4d8587158043b
larbibaraka <larbibaraka@gmail.com>
2022-04-11 01:30:35 +0100

    register new kafka client

--- billing/src/app.controller.ts
+++ billing/src/app.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import { EventPattern } from '@nestjs/microservices';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +9,11 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+
+  @EventPattern('order_created')
+  handleOrderCreated(data : any) {
+    this.appService.HandleOrderCreated(data.value)
   }
 }
--- billing/src/app.service.ts
+++ billing/src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { OrderCreatedEvent } from './order-created.event';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+
+  HandleOrderCreated(orderCreatedEvent : OrderCreatedEvent) {
+    console.log('=> ', orderCreatedEvent)
   }
 }
